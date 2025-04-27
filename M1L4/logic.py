b955ad96c7002950c3313650ffc9ed6ec2507e2b
4e6ypekGD <sokrutinruslan@gmail.com>
2025-04-27 21:21:48 +0300

    Add files via upload

--- M1L4/logic.py
+++ M1L4/logic.py
@@ -12,11 +12,14 @@ class Pokemon:
         self.img = self.get_img()
         self.name = self.get_name()
 
+        self.power = randint(30,70)
+        self.hp = randint(200,400)
+ 
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-          url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+          url = f'https://pokeapi.co/api/v2/pokemon/1{self.pokemon_number}'
           response = requests.get(url)
           if response.status_code == 200:
             data = response.json()
@@ -38,11 +41,50 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f'''Имя твоего покеомона: {self.name}
+        Сила покемона:{self.power}
+        Здоровье покемона:{self.hp}'''
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
+def attack(self, enemy):
+    if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+        chance = randint(1,5)
+    if chance == 1:
+        return "Покемон-волшебник применил щит в сражении"
+    if enemy.hp > self.power:
+        enemy.hp -= self.power
+        return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+    else:
+        enemy.hp = 0
+        return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    
+
+class Wizard(Pokemon):
+    def info(self):
+        return "У тебя покемон-волшебник" + super().info()
 
 
+class Fighter(Pokemon):
+    def attack(self, enemy):
+       super_power = randint(5,15)
+       self.power += super_power
+       result = super().attack(enemy)
+       self.сила -= super_power
+       return result + f"\nБоец применил супер-атаку силой:{super_power} "
+    
+    def info(self):
+        return "У тебя покемон-боец" + super().info()
+    
+
+wizard = Wizard('username1')
+fighter = Fighter('username2')
+
+print(wizard.info())
+print('#' * 10)
+print(fighter.info())
+print('#' * 10)
+print(wizard.attack(fighter))
+print(fighter.attack(wizard))
